8bad4eafc2ce1c9763a39bfdf6964e6a3db32c9d
JAGicana-43 <jagicana@spamast.edu.ph>
2026-04-22 19:23:01 +0800

    Step 16

--- CalculatorLab.js
+++ CalculatorLab.js
@@ -13,5 +13,6 @@ function calculateQuotient(num1, num2) {
     return num1 / num2;
   }
 }
-console.log(calculateQuotient(7, 11));
-console.log(calculateQuotient(3, 0));
+function calculateSquare(num) {
+  return Math.pow(num, 2);
+}
