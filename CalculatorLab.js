0454a5ff183e7ea753d005dd7c7fab888ea51c9a
JAGicana-43 <jagicana@spamast.edu.ph>
2026-04-22 19:20:17 +0800

    Step 15

--- CalculatorLab.js
+++ CalculatorLab.js
@@ -5,9 +5,13 @@ function calculateDifference(num1, num2) {
 function calculateProduct(num1, num2) {
   return num1 * num2;
 }
-// NEW: division function
+
 function calculateQuotient(num1, num2) {
-  return num1 / num2;
+  if (num2 === 0) {
+    return "Error: Division by zero";
+  } else {
+    return num1 / num2;
+  }
 }
 console.log(calculateQuotient(7, 11));
 console.log(calculateQuotient(3, 0));
